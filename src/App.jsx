dad7dafddb8a032e26e6cd1c9e8450b59f74b0f1
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-04 19:46:37 +0200

    implement react-course project-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import MainContent from "./components/MainContent";
+import NavBar from "./components/NavBar";
 import ProjectOne from "./components/ProjectOne";
 
-function App (){
-  return <ProjectOne></ProjectOne>
+function App() {
+  return (
+    <div className="container">
+      <NavBar></NavBar>
+      <MainContent></MainContent>
+    </div>
+  );
 }
 export default App;

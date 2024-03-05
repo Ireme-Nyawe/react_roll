2bf903e71276283fbd46c609efe1eca05e9782b0
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-05 15:18:51 +0200

    add contact component

---
+++ src/Challenge.jsx
@@ -0,0 +1,12 @@
+import Contacts from "./components/Contact";
+
+export default function Challenge() {
+  return (
+    <div className="contact-container">
+      <Contacts />
+      <Contacts />
+      <Contacts />
+      <Contacts />
+    </div>
+  );
+}
---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+export default function Contacts(){
+    return(
+        <div className="contact-card">
+            <img className="contact-image" src="./src/assets/react.svg" alt="card-image" />
+            <h2>react-logo</h2>
+            <div className="telephone">
+                <img src="" alt="" />
+                <span>+0987654567</span>
+            </div>
+            <div className="email">
+                <img src="" alt="" />
+                <span>contact@react.com</span>
+            </div>
+        </div>
+    );
+}

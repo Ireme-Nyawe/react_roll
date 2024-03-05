96898f5a2d875a097fb6184f83e6508de36e17ca
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-05 15:34:18 +0200

    add props to components

--- src/Challenge.jsx
+++ src/Challenge.jsx
@@ -3,10 +3,30 @@ import Contacts from "./components/Contact";
 export default function Challenge() {
   return (
     <div className="contact-container">
-      <Contacts />
-      <Contacts />
-      <Contacts />
-      <Contacts />
+      <Contacts 
+      img="./src/assets/react.svg" 
+      name="react-logo" 
+      phone="+67876546567" 
+      email="contact@react.com"
+      />
+      <Contacts 
+      img="./src/assets/bike.jpg" 
+      name="great-Bike" 
+      phone="+123456876543" 
+      email="contact@bike.com"
+      />
+      <Contacts 
+      img="./src/assets/flower.jpg" 
+      name="rwandan-flower" 
+      phone="+25078654535" 
+      email="contact@flower.com"
+      />
+      <Contacts 
+      img="./src/assets/teacher.png" 
+      name="teacher-move" 
+      phone="+13404040595" 
+      email="contact@teacher.com"
+      />
     </div>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,15 @@
-export default function Contacts(){
+export default function Contacts({img,name,phone,email}){
     return(
         <div className="contact-card">
-            <img className="contact-image" src="./src/assets/react.svg" alt="card-image" />
+            <img className="contact-image" src={img} />
             <h2>react-logo</h2>
             <div className="telephone">
                 <img src="" alt="" />
-                <span>+0987654567</span>
+                <span>{phone}</span>
             </div>
             <div className="email">
                 <img src="" alt="" />
-                <span>contact@react.com</span>
+                <span>{email}</span>
             </div>
         </div>
     );
